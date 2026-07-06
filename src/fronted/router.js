9318085441d2d3c8b2b50fc12c83f2a1054a30ad
aiincome16 <marketing.aiincomelab@gmail.com>
2026-07-06 21:17:45 +0200

    router.js aktualisieren

--- src/fronted/router.js
+++ src/fronted/router.js
@@ -1,20 +1,22 @@
 /************************************************
  * Facility OS 2.0
  * router.js
- *
- * Zentrale Navigation
  ************************************************/
 
-import { appState } from "./appState.js";
-
 import { renderLogin } from "./ui/loginUi.js";
-import { renderEmployeeDashboard } from "./ui/employeeDashboard.js";
-import { renderManagerDashboard } from "./ui/managerDashboard.js";
-import { renderAccountingDashboard } from "./ui/accountingDashboard.js";
-import { renderAdminDashboard } from "./ui/adminDashboard.js";
-import { renderCustomerDashboard } from "./ui/customerDashboard.js";
 import { renderObjectSelection } from "./ui/objectSelectionUi.js";
+import { renderEmployeeDashboard } from "./ui/employeeDashboard.js";
+
 import { renderRoomUi } from "./ui/roomUi.js";
+import { renderRoomWorkspace } from "./ui/roomWorkspaceUi.js";
+
+import { renderTaskUi } from "./ui/taskUi.js";
+import { renderMaterialUi } from "./ui/materialUi.js";
+import { renderTicketUi } from "./ui/ticketUi.js";
+import { renderMailboxUi } from "./ui/mailboxUi.js";
+import { renderNotificationUi } from "./ui/notificationUi.js";
+import { renderShiftUi } from "./ui/shiftUi.js";
+import { renderSettingsUi } from "./ui/settingsUi.js";
 
 class Router {
 
@@ -24,128 +26,13 @@ class Router {
 
     }
 
-    /********************************************
-     * Navigation
-     ********************************************/
-
     go(route) {
 
         this.currentRoute = route;
 
-        appState.update("ui", {
-
-            currentView: route
-
-        });
-
         this.render();
 
     }
-
-    /********************************************
-     * Routing
-     ********************************************/
-
-    render() {
-
-        const user =
-            appState.get("currentUser");
-
-        const role =
-            appState.get("currentRole");
-
-        const object =
-            appState.get("selectedObject");
-
-        switch (this.currentRoute) {
-
-            case "login":
-
-                renderLogin();
-
-                break;
-
-            case "dashboard":
-
-                if (!user) {
-
-                    this.go("login");
-
-                    return;
-
-                }
-
-                switch (role) {
-
-                    case "Mitarbeiter":
-
-                        renderEmployeeDashboard();
-
-                        break;
-
-                    case "Objektleiter":
-
-                        renderManagerDashboard();
-
-                        break;
-
-                    case "Buchhaltung":
-
-                        renderAccountingDashboard();
-
-                        break;
-
-                    case "Admin":
-
-                        renderAdminDashboard();
-
-                        break;
-
-                    case "Kunde":
-
-                        renderCustomerDashboard();
-
-                        break;
-
-                    default:
-
-                        renderLogin();
-
-                }
-
-                break;
-
-            case "objects":
-
-                renderObjectSelection();
-
-                break;
-
-            case "rooms":
-
-                if (!object) {
-
-                    this.go("objects");
-
-                    return;
-
-                }
-
-                renderRoomUi();
-
-                break;
-
-            default:
-
-                renderLogin();
-
-        }
-
-    }
-
-    /********************************************
-     * Helfer
-     ********************************************/
 
     refresh() {
 
@@ -153,9 +40,71 @@ class Router {
 
     }
 
-    getCurrentRoute() {
+    render() {
 
-        return this.currentRoute;
+        switch (this.currentRoute) {
+
+            case "login":
+                renderLogin();
+                break;
+
+            case "objects":
+                renderObjectSelection();
+                break;
+
+            case "dashboard":
+                renderEmployeeDashboard();
+                break;
+
+            case "rooms":
+                renderRoomUi();
+                break;
+
+            case "room":
+                renderRoomWorkspace();
+                break;
+
+            case "tasks":
+                renderTaskUi();
+                break;
+
+            case "materials":
+                renderMaterialUi();
+                break;
+
+            case "tickets":
+                renderTicketUi();
+                break;
+
+            case "mailbox":
+                renderMailboxUi();
+                break;
+
+            case "notifications":
+                renderNotificationUi();
+                break;
+
+            case "shift":
+                renderShiftUi();
+                break;
+
+            case "settings":
+                renderSettingsUi();
+                break;
+
+            default:
+
+                console.warn(
+
+                    "Unbekannte Route:",
+
+                    this.currentRoute
+
+                );
+
+                renderEmployeeDashboard();
+
+        }
 
     }
 
