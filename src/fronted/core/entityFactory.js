73ee966eb8216de3469409c2e4992ca53e1302d6
aiincome16 <marketing.aiincomelab@gmail.com>
2026-07-06 12:56:12 +0200

    entityFactory.js erstellen

---
+++ src/fronted/core/entityFactory.js
@@ -0,0 +1,46 @@
+/************************************************
+ * Facility OS Platform
+ * entityFactory.js
+ *
+ * Standardstruktur aller Datenobjekte
+ ************************************************/
+
+export function createEntity(data = {}) {
+
+    const now = new Date().toISOString();
+
+    return {
+
+        id: crypto.randomUUID(),
+
+        organizationId: null,
+
+        objectId: null,
+
+        buildingId: null,
+
+        floorId: null,
+
+        areaId: null,
+
+        roomId: null,
+
+        active: true,
+
+        deleted: false,
+
+        status: "active",
+
+        createdBy: null,
+
+        createdAt: now,
+
+        updatedBy: null,
+
+        updatedAt: now,
+
+        ...data
+
+    };
+
+}
