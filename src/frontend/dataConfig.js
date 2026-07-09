0757b9b5cb7d32abed00134f48a74ed4f9c1d1de
aiincome16 <marketing.aiincomelab@gmail.com>
2026-07-09 14:47:31 +0200

    dataConfig.js aktualisieren

--- src/frontend/dataConfig.js
+++ src/frontend/dataConfig.js
@@ -1,11 +1,7 @@
 /************************************************
  * Facility OS 2.0
  * dataConfig.js
- *
- * Zentrale Datenquellen
  ************************************************/
-
-import { APP_CONFIG } from "./config.js";
 
 export const DATA_CONFIG = {
 
@@ -19,58 +15,10 @@ export const DATA_CONFIG = {
 
     materials: "./data/materials.json",
 
-    shifts: "./data/shifts.json",
-
-    qrCodes: "./data/qrCodes.json",
+    workOrders: "./data/workOrders.json",
 
     notifications: "./data/notifications.json",
 
     customerRequests: "./data/customerRequests.json"
 
 };
-
-/************************************************
- * API Konfiguration
- ************************************************/
-
-export const API_CONFIG = {
-
-    enabled: APP_CONFIG.useBackendApi,
-
-    baseUrl: APP_CONFIG.apiBaseUrl
-
-};
-
-/************************************************
- * Google Sheets
- ************************************************/
-
-export const SHEETS_CONFIG = {
-
-    enabled: APP_CONFIG.useGoogleSheets,
-
-    scriptUrl: APP_CONFIG.googleScriptUrl
-
-};
-
-/************************************************
- * Datenmodus
- ************************************************/
-
-export function getDataMode() {
-
-    if (APP_CONFIG.useBackendApi) {
-
-        return "api";
-
-    }
-
-    if (APP_CONFIG.useGoogleSheets) {
-
-        return "sheets";
-
-    }
-
-    return "json";
-
-}
